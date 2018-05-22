8e605c57aca2e82694cc152d34b4176ed095dbb8
mitchellunderwood <mitchellunderwood@rocketmail.com>
2018-05-22 12:56:05 -0700

    AJAX first request:

--- W6D3/skeleton/js/index.js
+++ W6D3/skeleton/js/index.js
@@ -1,16 +1,22 @@
 console.log("Hello from the JavaScript console!");
 
 
-  $.ajax({
-    type: 'GET',
-    url: "http://api.openweathermap.org/data/2.5/weather?q=new%20york,US&appid=bcb83c4b54aee8418983c2aff3073b3b",
-    success(data) {
-        console.log("We have your weather!")
-        console.log(data);
-      },
-    error() {
-        console.error("An error occurred.");
-      },
-  });
+$.ajax({
+  type: 'GET',
+  url: "http://api.openweathermap.org/data/2.5/weather?q=new%20york,US&appid=bcb83c4b54aee8418983c2aff3073b3b",
+  success(data) {
+      console.log("We have your weather!")
+      console.log(data);
+    },
+  error() {
+      console.error("An error occurred.");
+    },
+});
 
-// Add another console log here, outside your AJAX request
+
+console.log("AJAX has been dispatched!");
+
+// your AJAX request
+    // --> including a console.log for the data in the success callback
+
+// another console.log down here
